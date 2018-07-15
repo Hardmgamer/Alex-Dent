7a13c95c9d0d98b379bcded996e92f9b9fa0c840
Mohammad Ashraf <Mohamed.ashraf881999@gmail.com>
2018-07-15 22:14:47 +0200

    70% Product Page Design Finished

---
+++ js/ProductBox.js
@@ -0,0 +1,8 @@
+function prodBox(){
+  document.getElementById('productDet').style.display = "block";
+
+}
+function HideProdBox(){
+  document.getElementById('productDet').style.display = "none";
+
+}
