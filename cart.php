48d50498d1f698062b27fb0f2f0797dfeba545a1
xTrimy <Mohamed.ashraf881999@gmail.com>
2018-07-19 01:51:02 +0200

    Huge Changes

--- cart.php
+++ cart.php
@@ -1,12 +1,14 @@
 <html>
  <head>
   <title> Your cart - Alexdent </title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="./Stylesheets/Main.css">
   <link rel="stylesheet" type="text/css" href="./Stylesheets/usercart.css">
  </head>
-  <?php include('./header.html'); ?>
-  <?php include('./footer.html'); ?>
+
   <body>
+    <?php include('./header.html'); ?>
+
     <form class="cart" method="post" action="">
       <table class="cart-table">
         <thead>
@@ -14,33 +16,36 @@
             <th class="first">Product</th>
             <th>Code</th>
             <th>Quantity</th>
-            <th class="last">In stock</th>
           </tr>
         </thead>
         <tbody>
           <tr class="cart-item">
             <th class="Product">
-              <a class="image" href="./products.php/">
+              <a class="image" href="./products.php">
                 <img alt="Product name" src="./images/products/3.jpeg" >
+                <div class="cart-name">
+                  Product name
+                </div>
               </a>
-              <div class="Pname">
-                <span class="cart-name">
-                  <a href="#">Product name</a>
-                </span>
-              </div>
             </th>
             <th class="code">
               <spin>XXXX</spin>
-            </td>
+            </th>
             <th class="quantity">
               <input type="text">
-            </td>
-            <th class="Av">
-              <spin>In stock</spin>
             </th>
           </tr>
         </tbody>
       </table>
+      <div class="table-end">
+        <div class="table-butt">
+          <input style="background:#01b1b0" type="submit" class="tbutton" value="Next" name="submit">
+          <div style="background:#b90909" class="tbutton">cancel</div>
+
+        </div>
+      </div>
     </form>
+
+        <?php include('./footer.html'); ?>
   </body>
 </html>
