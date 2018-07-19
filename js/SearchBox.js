b037b6933dec2e75c09677fe306356dfd795cc79
xTrimy <Mohamed.ashraf881999@gmail.com>
2018-07-19 02:04:26 +0200

    ...

---
+++ js/SearchBox.js
@@ -0,0 +1,9 @@
+function searchbox(){
+  document.getElementById('searchContainer').style.height = "60px";
+  document.getElementById('searchContainer').style.borderBottom = "3px solid #00cbba";
+}
+function searchboxhide(){
+  document.getElementById('searchContainer').style.height = "0px";
+  document.getElementById('searchContainer').style.borderBottom = "0px";
+
+}
