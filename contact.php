4d5316720dd5e129b70e3bfd0ba641f4fd35856c
xTrimy <Mohamed.ashraf881999@gmail.com>
2018-07-19 03:17:22 +0200

    ...

--- contact.php
+++ contact.php
@@ -1,6 +1,7 @@
 <html>
 <head>
   <title>Contact us</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="./Stylesheets/Main.css">
   <link rel="stylesheet" type="text/css" href="./Stylesheets/contact.css">
 </head>
