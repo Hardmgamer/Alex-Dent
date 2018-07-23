9b6aaac3e7084be4e459afccc5c09c3d8dac3154
Mohammad Salah <redmohammad22@gmail.com>
2018-07-24 00:23:32 +0300

    Products editing

--- ControlPanal/Products/products.php
+++ ControlPanal/Products/products.php
@@ -5,19 +5,33 @@
  <table>
   <thead>
     <tr>
-      <th class="first">Image</th>
+      <th>Image</th>
       <th>Name</th>
       <th>Company</th>
       <th>Availability</th>
+      <th></th>
     </tr>
   </thead>
   <tbody>
+    <tr class="pr">
+      <th>Image</th>
+      <th>Product Name</th>
+      <th>Company Name</th>
+      <th>In Stock</th>
+      <th><a href="#">Edit</a></th>
+    </tr>
     <tr>
       <th><input type="text" placeholder="link of the image"></th>
       <th><input type="text" placeholder="name of products"></th>
       <th><input type="text" placeholder="name of Company"></th>
-      <th><select><option value="InStock">Available</option></select>
+      <th>
+      <select>
+      <option value="InStock">Available</option>
+      <option value="OutOfStock">Out of stock</option>
+      <option value="ComingSoon">Coming soon</option>
+      </select>
       </th>
+      <th></th>
     </tr>
     <tr>
       <th></th>
--- productinfo.php
+++ productinfo.php
@@ -5,63 +5,24 @@
   <link rel="stylesheet" type="text/css" href="./Stylesheets/prodinfo.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 </head>
-<?php include('./header.html'); ?>
-<?php include('./footer.html'); ?>
 <body>
-  <div class="page">
-  <div class="photos">
-    <div class="image">
+  <?php include('./header.html'); ?>
+  <div class="body">
+    <div class="imageCont">
+      <div class="bimage">
+      </div>
+      <div class="simages">
+        <center>
+        <div class="simage"></div>
+        <div class="simage"></div>
+        <div class="simage"></div>
+        </center>
+      </div>
     </div>
-    <div class="minimage">
-    <div class="image1">
-    </div>
-    <div class="image2">
-    </div>
-    <div class="image3">
+    <div class="detCont">
+      <h3>Product Name</h3>
     </div>
   </div>
-  </div>
-    <div class="name">
-      <h3>Product name</h3>
-      <h4>Short descreption:</h4>
-    </div>
-    <div class="little">
-    <div class="descreption">
-      <p>Each. Anutex modelling wax sheets are used by the technician to create the preliminary denture base for the dentist to try in the patients mouth.</p>
-      <p>Catagory:<a href="#"><strong>Dental</strong></a></p>
-      <p>Code:<strong class="code">XXXXXX</strong></p>
-      <p>Avaliabilty:<strong class="is">In stock</strong></p>
-     </div>
-   </div>
-  <div class="buttons">
-    <div class="ProdButton"><p><span class="fas fa-cart-plus"></span>Add To Cart</p></div>
-    <div class="ProdButton"><p><span class="fas fa-file-invoice-dollar"></span>Inquiry</p></div>
-  </div>
-  <div class="sections">
-    <div class="descreptionsec"><strong>descreption</strong></div>
-    <div class="features"><strong>XXXX</strong></div>
-  </div>
-  <div class="Detials">
-    <h4><strong>FEATURES:</strong></h4>
-    <ul>
-      <li>Color: Translucent pink color.</li>
-      <li>Sheet size: 189mm x 89mm x 1.5mm thick.</li>
-      <li>Pack of 500g (24 sheets).</li>
-    </ul>
-  </div>
-  <div class="Benfits">
-    <h4><strong>BENEFITS:</strong></h4>
-    <ul>
-      <li>benfit.</li>
-      <li>benfit.</li>
-      <li>benfit.</li>
-      <li>benfit.</li>
-      <li>benfit.</li>
-      <li>benfit.</li>
-      <li>benfit.</li>
-      <li>benfit.</li>
-      <li>benfit.</li>
-    </ul>
-  </div>
+  <?php include('./footer.html'); ?>
 </body>
 </html>
